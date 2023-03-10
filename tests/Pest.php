fc76881db979d9a70f7082b1e98b4e7cd18e0c3a
Lars Bergvall <lars.bergvall@academedia.se>
2023-03-10 13:54:13 +0100

    added custom expectations for easier testing

--- tests/Pest.php
+++ tests/Pest.php
@@ -32,3 +32,30 @@ expect()->extend('toHaveIncludedCount', function (int $expectedCount) {
 
     return $expectation;
 });
+
+expect()->extend('toHaveRelationship', function (string $relationName, int|string $id, string $type) {
+    $expectation = expect($this->value)->toHaveKey("relationships.$relationName");
+
+    expect(data_get($this->value, "relationships.$relationName.data"))
+        ->toHaveKey('id', $id)
+        ->toHaveKey('type', $type);
+
+    return $expectation;
+});
+
+expect()->extend('toHaveRelationships', function (string $relationName, array $ids, string $type) {
+    $expectation = expect($this->value)->toHaveKey("relationships.$relationName.data");
+
+    $relationships = data_get($this->value, "relationships.$relationName.data");
+    $found = 0;
+
+    foreach ($relationships as $relationship) {
+        expect($relationship)->toHaveKey('type', $type)
+            ->and($relationship['id'])->toBeIn($ids);
+        $found++;
+    }
+
+    expect($found)->toEqual(count($ids));
+
+    return $expectation;
+});
