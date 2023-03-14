c4bfe3a31fcfe4ffc3554f4401bd618b8d82284d
Lars Bergvall <35922396+larsmbergvall@users.noreply.github.com>
2023-03-14 15:18:52 +0100

    added paginator support (#3)
    * it now supports paginators
    
    * use concrete paginator
    
    * fixes
    
    * minimum should be 9.34

--- src/JsonApi/JsonApiResourceCollection.php
+++ src/JsonApi/JsonApiResourceCollection.php
@@ -2,9 +2,12 @@
 
 namespace Larsmbergvall\JsonApiResourcesForLaravel\JsonApi;
 
+use Illuminate\Contracts\Pagination\Paginator as PaginatorContract;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 use InvalidArgumentException;
 use JsonSerializable;
 
@@ -18,17 +21,30 @@ class JsonApiResourceCollection implements JsonSerializable, Arrayable
     /** @var class-string<Model> */
     protected string $modelClass;
 
+    protected Collection $models;
+
+    protected ?PaginatorContract $paginator = null;
+
+    protected array $links;
+
     /** @var Collection<int, JsonApiResource<T>> */
     private Collection $jsonApiResources;
 
     /**
-     * @param  Collection<int, T>  $models
+     * @param  Paginator<T>|PaginatorContract<T>|Collection<int, T>  $models
      * @param  class-string|null  $model
      */
-    public function __construct(protected Collection $models, ?string $model = null)
+    public function __construct(Collection|Paginator|PaginatorContract $models, ?string $model = null)
     {
-        if (! $model && $this->models->isEmpty()) {
+        if (! $model && $models->isEmpty()) {
             throw new InvalidArgumentException('There must be either a $model argument or at least one item in $resources!');
+        }
+
+        if ($models instanceof PaginatorContract) {
+            $this->models = collect($models->items());
+            $this->paginator = $models;
+        } else {
+            $this->models = $models;
         }
 
         if (! $model) {
@@ -44,11 +60,11 @@ class JsonApiResourceCollection implements JsonSerializable, Arrayable
     }
 
     /**
-     * @param  Collection<int, T>  $models
+     * @param  Paginator<T>|PaginatorContract<T>|Collection<int, T>  $models
      * @param  class-string|null  $model
      * @return JsonApiResourceCollection<T>
      */
-    public static function make(Collection $models, ?string $model = null): static
+    public static function make(Collection|Paginator|PaginatorContract $models, ?string $model = null): static
     {
         return new static($models, $model);
     }
@@ -66,6 +82,10 @@ class JsonApiResourceCollection implements JsonSerializable, Arrayable
 
         if ($this->withIncluded) {
             $payload['included'] = $this->loadIncluded()->jsonSerialize();
+        }
+
+        if ($this->paginator) {
+            $payload['links'] = $this->linksFromPaginator($this->paginator);
         }
 
         return $payload;
@@ -97,5 +117,17 @@ class JsonApiResourceCollection implements JsonSerializable, Arrayable
     private function includedFromResource(JsonApiResource $resource): Collection
     {
         return $resource->loadIncluded()->keyBy(fn (JsonApiResource $r) => $r->identifier());
+    }
+
+    private function linksFromPaginator(Paginator|PaginatorContract $paginator): array
+    {
+        $serializedPaginator = $paginator->jsonSerialize();
+
+        return array_map(fn (string $link) => Str::replace('\\/', '/', $link), array_filter([
+            'first' => data_get($serializedPaginator, 'first_page_url'),
+            'last' => data_get($serializedPaginator, 'last_page_url'),
+            'prev' => data_get($serializedPaginator, 'prev_page_url'),
+            'next' => data_get($serializedPaginator, 'next_page_url'),
+        ]));
     }
 }
